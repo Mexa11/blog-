2eba411f8e6b3e45af82554ba6d7d62153c5733c
Mexa11 <gaffurovmehriddin@gmail.com>
2023-06-08 17:22:50 +0300

    Mening birinchi proyektim

--- app/models.py
+++ app/models.py
@@ -16,3 +16,34 @@ class ProfileData(BaseModel):
 
     def __str__(self):
         return self.name
+
+
+class SocialLinks(BaseModel):
+    name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=50)
+    url = models.URLField(max_length=240)
+    order = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ("order",)
+
+    def str(self):
+        return self.name
+
+
+class Posts(BaseModel):
+    title = models.CharField(max_length=250)
+    image = models.ImageField(upload_to="posts")
+    body = models.TextField()
+
+    def str(self):
+        return self.title
+
+
+class About(BaseModel):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="about")
+    description = models.TextField()
+
+    def str(self):
+        return self.title
